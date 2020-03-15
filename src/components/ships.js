9214b3cd6a9891f33cf78748459a66931c81ca0e
Tim K-G <tim.theodor@gmail.com>
2020-03-15 04:45:36 +0100

    adds key to ships

--- src/components/ships.js
+++ src/components/ships.js
@@ -15,7 +15,7 @@ export default (props) => {
             }
 
             {props.starships.map((ship) => (
-                <Ship {...ship} />
+                <Ship key={ship.url} {...ship} />
             ))}
 
             {props.fetchNext && 
